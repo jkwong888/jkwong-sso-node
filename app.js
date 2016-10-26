3eb7746cf2eaf7730c2214d27596e48549490406
jkwong888 <jkwong@ca.ibm.com>
2016-10-26 15:15:04 -0400

    ensure authenticated

--- app.js
+++ app.js
@@ -93,13 +93,11 @@ app.get('/logout', function(req, res) {
       });
 
 // serve the files out of ./public as our main files
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res, next) {
+app.get('/public', function(req, res, next) {
     ensureAuthenticated(req, res, next);
     next();
-}), function(req, res) {
-    app.use(express.static(__dirname + '/public'));
 };
 
 // get the app environment from Cloud Foundry
